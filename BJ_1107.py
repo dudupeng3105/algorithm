10fec8b5cc584dbe9afb4706a933b48480e0e1a6
donguk3105 <donguk3105@naver.com>
2022-01-16 11:46:06 +0900

    "백준 1107번 품"

--- BJ_1107.py
+++ BJ_1107.py
@@ -1,26 +1,21 @@
-##########1107번####################
-possible_button = [0,1,2,3,4,5,9]
-want_channel = 5457
-now_channel = 100
-cnt = 0
-def good_case(want_channel, N, cnt):
-    head_number = want_channel // (10**N)
-    if head_number in possible_button:
-        cnt += 1 # 1번 클릭
-        want_channel = want_channel % (10**N)
-        bad_case_flag = 0
-        N = N -1
-    else:
-        bad_case_flag = 1
-
-    return want_channel, cnt, bad_case_flag, N
-
-def bad_case(want_channel, N, cnt):
-    ## 남은 수 올라가면서
-    head_number = want_channel // (10 ** N)
-    head_number += 1
-    if head_number not in possible_button:
-        head_number += 1
+def checker(num):
+    num = list(str(num))
+    for i in num:
+        if i in broken_button_list:
+            return False
+    return True
 
 
-    ## 남은 수 내려가면서
+want_channel = int(input())
+broken_button_num = int(input())
+if broken_button_num != 0:
+    broken_button_list = list(input().split(" "))
+else:  # 고장난 버튼 없을 때
+    broken_button_list = []
+
+result = abs(want_channel - 100)
+
+for i in range(1000001):
+    if checker(i):
+        result = min(result, len(str(i)) + abs(i - want_channel))
+print(result)
