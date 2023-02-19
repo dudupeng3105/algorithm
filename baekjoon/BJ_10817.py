5ff68458a25bbc94c405e99fa1f3ce156c01cf37
donguk3105 <donguk3105@naver.com>
2023-02-19 15:58:44 +0900

    algo

---
+++ baekjoon/BJ_10817.py
@@ -0,0 +1,45 @@
+# import sys
+#
+# input = sys.stdin.readline
+
+# 25083
+print('         ', ",", 'r', "'", '"', '7', sep='')
+print('r`-_   ,', "'", '  ,/', sep='')
+print(' \. ". L_r', "'", sep='')
+print('   `~\/')
+print('      |')
+print('      |')
+
+# 11718
+# while True:
+#     try:
+#         print(input())
+#     except EOFError:
+#         break
+
+# 3003
+# standard_chess = [1, 1, 2, 2, 2, 8]
+# my_chess = list(map(int, input().split()))
+# for i in range(6):
+#     print(standard_chess[i] - my_chess[i], end=' ')
+
+# 2441
+# N = int(input())
+# for i in range(N):
+#     print(' '*i, end='')
+#     print('*' * (N-i))
+
+# 7287
+# print('481')
+# print('donguk3105')
+
+# 2440
+# N = int(input())
+# for i in range(N):
+#     print('*' * (N-i))
+
+# 10817
+# abc_lst = list(map(int, input().split()))
+# abc_lst.sort()
+#
+# print(abc_lst[1])
